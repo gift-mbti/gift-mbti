38e57e8da2c7616ce8cbb8698e7c091f9815ce8a
hovelopin <kimhojin3714@naver.com>
2022-09-22 22:08:11 +0900

    setting : emotion-global-style 설정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,19 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { css, Global } from '@emotion/react'
+import reset from '../styles/reset'
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Global
+        styles={css`
+          ${reset}
+        `}
+      />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
---
+++ src/styles/reset.ts
@@ -0,0 +1,13 @@
+import { css } from '@emotion/react'
+
+const reset = css`
+  body {
+    background-color: white;
+    color: black;
+    width: 375px;
+    border: 1px solid black;
+    margin: 0 auto;
+  }
+`
+
+export default reset
