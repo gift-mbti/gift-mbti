c99c22e031c018a0333e22ad22a999657f73db2e
ejaman <leegm17@naver.com>
2022-09-24 03:25:00 +0900

    feat: result card 부분 완성

--- src/pages/result/[...type].tsx
+++ src/pages/result/[...type].tsx
@@ -1,84 +1,155 @@
 import styled from '@emotion/styled';
 import SEO from '../../components/SEO';
 
-const Container = styled.div`
-  /* position: absolute; */
+const Wrapper = styled.div`
+  position: absolute;
   width: 375px;
   height: 2051px;
-  left: 410px;
-  top: 80px;
-  background-color: red;
-  /* background: #ffffff; */
 `;
 
-const CardContainer = styled.div`
-  /* position: absolute; */
-  width: 375px;
-  height: 408.87px;
-  left: 410px;
-  top: 80px;
-  z-index: auto;
-  background-image: url('/img/bg_result.svg');
-`;
-
+const Background = styled.img``;
 const Card = styled.div`
   position: absolute;
   width: 279px;
   height: 389px;
-  left: 457px;
-  top: 176px;
+  left: 48px;
+  top: 46px;
 
   background: #ffffff;
   box-shadow: 0px 10px 40px rgba(106, 106, 106, 0.25);
   border-radius: 20px;
 `;
-const Image = styled.div`
+
+const Image1 = styled.div`
   position: absolute;
   width: 200px;
   height: 136px;
-  left: 506px;
-  top: 225px;
+  left: 48px;
+  top: 99px;
   background: url('/img/image 1.svg');
 `;
+
 const CardTitle = styled.p`
   position: absolute;
   width: 111px;
   height: 55px;
-  left: 484px;
-  top: 156px;
-
+  left: 26px;
+  top: 10px; // 디자인: 30px하니까 너무 멂 물어보기!
   font-family: 'Pretendard';
   font-style: normal;
   font-weight: 800;
   font-size: 25px;
   line-height: 100%;
   /* or 25px */
+
   letter-spacing: 0.02em;
+
   color: #000000;
 `;
-const Vetor = styled.div`
+
+const QR = styled.img`
   position: absolute;
-  left: 57.45%;
+  left: 228px;
   right: 40.45%;
   top: 7.46%;
   bottom: 91.37%;
+`;
 
-  background: url('/img/Vector.svg');
+const Title = styled.p`
+  position: absolute;
+  width: 25px;
+  height: 14px;
+  left: 26px;
+  top: 248px;
+  font-family: 'Pretendard';
+  font-style: normal;
+  font-weight: 700;
+  font-size: 14px;
+  line-height: 100%;
+  /* identical to box height, or 14px */
+  color: #393939;
+`;
+
+const Tag1 = styled.div`
+  box-sizing: border-box;
+
+  /* Auto layout */
+
+  display: flex;
+  flex-direction: row;
+  align-items: flex-start;
+  padding: 5px 10px;
+  gap: 10px;
+
+  position: absolute;
+  width: 71px;
+  height: 28px;
+  left: 26px;
+  // 디자인 : 높이 조정해둠 물어보기
+  /* top: 274px; */
+  top: 284px;
+
+  background: #ffffff;
+  border: 1.5px solid #74b6fe;
+  border-radius: 18px;
+
+  font-family: 'Pretendard';
+  font-style: normal;
+  font-weight: 800;
+  font-size: 12px;
+  line-height: 148%;
+
+  color: #74b6ff;
+`;
+const Tag2 = styled(Tag1)`
+  left: 102px;
+`;
+const Tag3 = styled(Tag1)`
+  left: 177px;
+`;
+
+// 디자인 : 뭔가 이상함
+const CardText = styled.div`
+  position: absolute;
+  width: 200px;
+  height: 30px;
+  left: 26px;
+  top: 322px;
+
+  font-family: 'Pretendard';
+  font-style: normal;
+  font-weight: 600;
+  font-size: 12px;
+  line-height: 12px;
+
+  color: #929090;
 `;
 
 const Result = ({ params }: any) => {
   // url에 제목 넣으면 seo에 좋기 떄문
   const [type] = params || [];
   return (
-    <Container>
+    <Wrapper>
       <SEO title={type} />
-      <CardContainer />
+      <Background src="/img/bg_result.svg" alt="result_card" />
       <Card>
-        <CardTitle>프로 지갑지킴이</CardTitle>
-        <Vetor />
-        <Image />
+        <CardTitle>
+          프로
+          <br />
+          지갑지킴이
+        </CardTitle>
+        <QR src="/img/Vector.svg" alt="result_card" />
+        <Image1 />
+        <Title>태그</Title>
+        <Tag1>#가격비교</Tag1>
+        <Tag2>#계산적</Tag2>
+        <Tag3>#신중함</Tag3>
+        <CardText>
+          전시된 핸드폰으로 본인의 유형 카드를
+          <br /> 인식하면 AR로 캐릭터를 만나볼 수 있어요!
+        </CardText>
       </Card>
-    </Container>
+    </Wrapper>
   );
 };
 
