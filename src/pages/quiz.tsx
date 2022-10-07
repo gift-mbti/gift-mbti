c1adfdd46c5e8bf6047c09c30e5103aef7496e0d
hovelopin <kimhojin3714@naver.com>
2022-10-08 00:08:50 +0900

    feat : 기본적인 스타일 변경 및 modal 기능 추가

--- src/pages/quiz.tsx
+++ src/pages/quiz.tsx
@@ -2,8 +2,10 @@ import styled from '@emotion/styled';
 import React, { useMemo, useState } from 'react';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
+import Modal from 'react-modal';
 import QuizArticle from '../components/QuizArticle';
 import items from '../data/quiz';
+import ModalStyle from '../styles/modalStyle';
 
 const Wrapper = styled.div``;
 const QuizHeader = styled.div`
@@ -16,6 +18,7 @@ const QuizLogoImg = styled.img`
   width: 56.55px;
   height: 50px;
   margin-left: 22px;
+  cursor: pointer;
 `;
 const QuestionCountContainer = styled.p`
   width: 83px;
@@ -23,15 +26,52 @@ const QuestionCountContainer = styled.p`
   border: 2px solid #dedede;
   border-radius: 8px;
   text-align: center;
-  padding-top: 7px;
-  /* 호진TODO : P태그에 기본적으로 margin top 요소가 있는 이유? */
   margin-top: 0px;
   margin-right: 40px;
   font-weight: 800;
   font-size: 14px;
-  line-height: 148%;
+  line-height: 35px;
   letter-spacing: 0.02em;
   color: #575757;
+`;
+
+const ModalWrapper = styled.div``;
+
+const ModalTitle = styled.p`
+  text-align: center;
+  font-weight: 700;
+  font-size: 16px;
+  line-height: 100%;
+  margin-top: 40px;
+`;
+
+const ModalBackBtn = styled.button`
+  display: block;
+  width: 282px;
+  height: 42px;
+  margin: 0 auto;
+  margin-top: 32px;
+  background-color: #fd6463;
+  border-radius: 10px;
+  font-weight: 800;
+  font-size: 13px;
+  color: white;
+  border: none;
+  cursor: pointer;
+`;
+const ModalCancelBtn = styled.button`
+  display: block;
+  width: 282px;
+  height: 42px;
+  margin: 0 auto;
+  margin-top: 9px;
+  background-color: #ebebeb;
+  opacity: 0.8;
+  border-radius: 10px;
+  border: none;
+  font-weight: 700;
+  font-size: 13px;
+  cursor: pointer;
 `;
 
 const quiz = () => {
@@ -39,7 +79,7 @@ const quiz = () => {
   const [questions, setQuestion] = useState<number>(8);
   const [finish, setFinish] = useState<boolean>(false);
   const currentData = useMemo(() => items[steps], [steps]);
-
+  const [open, isOpen] = useState<boolean>(false);
   const router = useRouter();
 
   const handleClickNextStep = () => {
@@ -51,13 +91,31 @@ const quiz = () => {
       router.push('/loading');
     }
   };
+  const handleClickModal = () => {
+    isOpen(true);
+  };
+  const handleCancelBtn = () => {
+    isOpen(false);
+  };
+  const handleMoveToHomeBtn = () => {
+    router.push('/');
+  };
 
   return (
     <Wrapper>
       <QuizHeader>
-        <Link href="/">
-          <QuizLogoImg src="/img/quizLogo.svg" alt="quizLogo" />
-        </Link>
+        <QuizLogoImg
+          src="/img/quizLogo.svg"
+          alt="quizLogo"
+          onClick={handleClickModal}
+        />
+        <Modal isOpen={open} style={ModalStyle} ariaHideApp={false}>
+          <ModalWrapper>
+            <ModalTitle>처음으로 돌아가시겠어요?</ModalTitle>
+            <ModalBackBtn onClick={handleMoveToHomeBtn}>돌아가기</ModalBackBtn>
+            <ModalCancelBtn onClick={handleCancelBtn}>취소</ModalCancelBtn>
+          </ModalWrapper>
+        </Modal>
         <QuestionCountContainer>남은 문항 {questions}</QuestionCountContainer>
       </QuizHeader>
       <QuizArticle items={currentData.qna} onNextStep={handleClickNextStep} />
---
+++ src/styles/modalStyle.ts
@@ -0,0 +1,14 @@
+const ModalStyle = {
+  overlay: {
+    backgroundColor: 'rgba(0,0,0,0.5)',
+  },
+  content: {
+    width: '321px',
+    height: '204px',
+    padding: '0',
+    margin: 'auto',
+    overflow: 'auto',
+  },
+};
+
+export default ModalStyle;
