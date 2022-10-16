a113656533be56db95b3842db353966de0e06bab
ejaman <leegm17@naver.com>
2022-10-17 03:53:50 +0900

    feat: 모든 유형 보기 페이지 생성

---
+++ src/pages/results.tsx
@@ -0,0 +1,18 @@
+import styled from '@emotion/styled';
+import SEO from '../components/SEO';
+
+const Nav = styled.nav``;
+const NavButton = styled.button``;
+
+const results = () => {
+  return (
+    <div>
+      <SEO type="모든 유형 한번에 보기" />
+      <Nav>
+        <NavButton>1</NavButton>
+      </Nav>
+    </div>
+  );
+};
+
+export default results;
