d87efb79bcd7ca9ca7ba454acd57701c24b2a5cc
ejaman <leegm17@naver.com>
2022-10-18 20:14:22 +0900

    feat: 스타일 체크

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,6 @@ import { css, Global, ThemeProvider } from '@emotion/react';
 import { SnackbarProvider } from 'notistack';
 import reset from '../styles/reset';
 import theme from '../styles/themes';
-import '../public/static/fonts/style.css';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
